88fb7d40fc9bd1db6e4aeebf8d0388c6a62df05d
Jessica Gladstone <jcg321@comcast.net>
2018-10-17 20:19:07 +0000

    automatically backed up by learn

---
+++ nyc_pigeon_organizer.rb
@@ -0,0 +1,16 @@
+
+def nyc_pigeon_organizer(data)
+  hash = {}
+  data.each do |key, value|           # 1st level
+    value.each do |char, name|        # 2nd level
+      name.each do |bird|             # 3rd level - get bird names
+        hash[bird] ||= {}             # put birds in hash using assignment operator
+        if !hash[bird].has_key?(key)  #check to see if hash doesn't have key, then add key
+          hash[bird][key] = []
+        end
+        hash[bird][key] << char.to_s  #push characteristics to key and change to string
+      end
+    end
+  end
+hash
+end
